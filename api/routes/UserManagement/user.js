baab5a33d6149ba691a1c2dd4ac85888ddee3cf0
samiramrullah <samiramrullah@gmail.com>
2023-05-26 20:15:05 +0530

    Deployment Testing

--- api/routes/UserManagement/user.js
+++ api/routes/UserManagement/user.js
@@ -29,8 +29,10 @@ router.post('/register', async (req, res, next) => {
 router.post('/login', async (req, res, next) => {
     try {
         const { email, password } = req.body;
+        
         if (!email || !password || !validator.isEmail(email)) return res.status(400).json({ message: 'Invalid Data' });
         const user = await userSchema.findOne({ email });
+        console.log('Deployment Testing');
        
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ message: 'Invalid email or password.' });
